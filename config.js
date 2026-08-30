a6d9cd6e31cf5ef31b75a24002b4526b75aff614
Jingpo <128562692+Jingpo15102006@users.noreply.github.com>
2026-08-30 15:26:52 +0700

    fix bug

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // In local development, it will default to localhost:3000.
 // When deployed, change this string to your backend's deployed URL.
 const CONFIG = {
-    API_BASE: 'https://jingjang-fullstack.onrender.com'
+    API_BASE: 'https://jingjang-store-backend.onrender.com'
 };
